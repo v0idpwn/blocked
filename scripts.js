ce7d6f62445d3c3b4ec28d6576d28172702b521e
v0idpwn <v0idpwn@gmail.com>
2021-08-05 01:55:31 +0300

    Display blocks count for last 24 hours and ever
    Asynchronously fetch and update block counts from lambda function.

--- scripts.js
+++ scripts.js
@@ -12,6 +12,21 @@ function random_namecall() {
   return names[Math.floor(Math.random()*names.length)]
 }
 
+function get_counts_async() {
+  var xmlHttp = new XMLHttpRequest();
+  xmlHttp.onreadystatechange = function() { 
+      if (xmlHttp.readyState == 4 && xmlHttp.status == 200){
+        const resp = JSON.parse(xmlHttp.responseText);
+        document.getElementById('today').innerHTML = resp.last_24h;
+        document.getElementById('ever').innerHTML = resp.forever;
+      }
+  }
+
+  xmlHttp.open("GET", "https://us-central1-hitcount-321920.cloudfunctions.net/hitcount-plus-one-1", true); // true for asynchronous 
+  xmlHttp.send(null);
+}
+
 window.onload = function() {
   document.getElementById('namecall').innerHTML = random_namecall();
+  get_counts_async();
 };
